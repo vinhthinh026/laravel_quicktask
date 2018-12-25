4ed7d283b00a36071224d0ea0dab8b87208be01b
ThinhVinh <vinhthinh026@gmail.com>
2018-12-25 14:36:54 +0700

    pull create database

---
+++ app/Models/Task.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = "tasks";
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        'name',
+    ];
+}
